fcb5399725bf56d983a1dc78dafd6be9d72f05dd
Davion3005 <45590385+Davion3005@users.noreply.github.com>
2022-06-15 16:04:56 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
-function App() {
+import { Route } from "react-router-dom";
+import { Routes } from "react-router-dom";
 
-  return (
-      <div></div>
-  );
+import AllMeetupsPage from "./pages/AllMeetups";
+import NewMeetupsPage from "./pages/NewMeetups";
+import FavoritesPage from "./pages/Favorites";
+import MainNavigation from "./components/layout/MainNavigation";
+
+function App() {
+    return (
+        <div>
+            <MainNavigation />
+            <Routes>
+                <Route path='/' element={<AllMeetupsPage />} />
+                <Route path='/new' element={<NewMeetupsPage />} />
+                <Route path='/favorites' element={<FavoritesPage />} />
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
