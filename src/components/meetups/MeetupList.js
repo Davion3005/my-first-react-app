0d461d827a8f5174ff36e41b5cf55cf66a8eb7ff
DanhNC <NguyenCongDanhNCDt@gmail.com>
2022-06-15 17:53:55 +0700

    udate

--- src/components/meetups/MeetupList.js
+++ src/components/meetups/MeetupList.js
@@ -1,8 +1,16 @@
 import classes from "./MeetupList.module.css";
+import MeetupItem from "./MeetupItem";
 
 function MeetupList(props) {
     return <ul className={classes.list}>
-        
+        { props.meetups.map((meetup) => <MeetupItem
+            key={meetup.id}
+            id={meetup.id}
+            title={meetup.title}
+            image={meetup.image}
+            address={meetup.address}
+            description={meetup.description}
+        />) }
     </ul>
 }
 
